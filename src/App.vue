39db108ae2ff281627098e63cada4a5b171d15cf
DrInnovatio <turing1723@gmail.com>
2022-02-15 19:30:42 +0900

    Initialization.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form>
+    <div class="help">
+      <p>This is some help text.</p>
+    </div>
+    <div class="fields">
+      <input type="text" placeholder="email">
+      <input type="text" placeholder="username">
+      <input type="password" placeholder="password">
+    </div>
+    <div class="buttons">
+      <button type="submit">Submit</button>
+    </div>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
