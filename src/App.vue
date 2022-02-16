06e071d84a56af2e2243870de65d9be2b4afaaf7
DrInnovatio <turing1723@gmail.com>
2022-02-16 20:35:11 +0900

    wrote again the code.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
-  <form>
-    <div class="help">
-      <p>This is some help text.</p>
-    </div>
-    <div class="fields">
-      <input type="text" placeholder="email">
-      <input type="text" placeholder="username">
-      <input type="password" placeholder="password">
-    </div>
-    <div class="buttons">
-      <button type="submit">Submit</button>
-    </div>
-  </form>
+  <app-form/>
 </template>
 
 <script>
 
+import AppForm from './components/Form.vue'
+
 export default {
   name: 'App',
+  components:{
+    AppForm,
+  }
   
 };
 </script>
